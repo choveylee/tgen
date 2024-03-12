2439831e95dad62b6c2304659f17efd3efe95328
lidonglin@rcrai.com <lidonglin@rcrai.com>
2024-03-12 21:01:30 +0800

    update template

--- template/internal/model/mysql/mysql.go
+++ template/internal/model/mysql/mysql.go
@@ -6,79 +6,79 @@
  * @Date: 2023/11/15 21:22
  */
 
- package dbmodel
+package dbmodel
 
- import (
-	 "context"
-	 "fmt"
- 
-	 "github.com/choveylee/tcfg"
-	 "github.com/choveylee/tdb"
-	 "github.com/choveylee/terror"
-	 "github.com/choveylee/tlog"
-	 _ "github.com/golang-migrate/migrate/v4/database/mysql"
-	 _ "github.com/golang-migrate/migrate/v4/source/file"
- 
-	 "{{domain}}/{{app_name}}/internal/const"
- )
- 
- var (
-	 runMode string
- 
-	 serverClient *tdb.MysqlClient
- )
- 
- func InitMysqlModel(ctx context.Context) *terror.Terror {
-	 runMode = tcfg.DefaultString(tcfg.LocalKey("RUN_MODE"), constant.RunModeRelease)
- 
-	 serverDsn, err := tcfg.String(fmt.Sprintf("%s::%s", runMode, tcfg.LocalKey("SERVER_MYSQL_DSN")))
-	 if err != nil {
-		 errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%s::%s) err (cfg string %v).",
-			 runMode, "SERVER_MYSQL_DSN", err)
- 
-		 errx := terror.NewRawTerror(ctx, err, errMsg)
- 
-		 return errx
-	 }
- 
-	 if runMode == constant.RunModeDebug {
-		 serverClient, err = tdb.NewMysqlClientWithLog(ctx, serverDsn)
-	 } else {
-		 serverClient, err = tdb.NewMysqlClient(ctx, serverDsn)
-	 }
-	 if err != nil {
-		 errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%s) err (new mysql client %v).",
-			 serverDsn, err)
- 
-		 errx := terror.NewRawTerror(ctx, err, errMsg)
- 
-		 return errx
-	 }
- 
-	 maxIdleConns := tcfg.DefaultInt(tcfg.LocalKey("MYSQL_MAX_IDLE_CONNS"), 10)
- 
-	 err = serverClient.SetMaxIdleConns(maxIdleConns)
-	 if err != nil {
-		 errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%d) err (set max idle conns %v).",
-			 maxIdleConns, err)
- 
-		 errx := terror.NewRawTerror(ctx, err, errMsg)
- 
-		 return errx
-	 }
- 
-	 maxOpenConns := tcfg.DefaultInt(tcfg.LocalKey("MYSQL_MAX_OPEN_CONNS"), 100)
- 
-	 err = serverClient.SetMaxOpenConns(maxOpenConns)
-	 if err != nil {
-		 errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%d) err (set max open conns %v).",
-			 maxOpenConns, err)
- 
-		 errx := terror.NewRawTerror(ctx, err, errMsg)
- 
-		 return errx
-	 }
- 
-	 return nil
- }
- 
+import (
+	"context"
+	"fmt"
+
+	"github.com/choveylee/tcfg"
+	"github.com/choveylee/tdb"
+	"github.com/choveylee/terror"
+	"github.com/choveylee/tlog"
+	_ "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	constant "dev.choveylee.top/tgen/template/internal/const"
+)
+
+var (
+	runMode string
+
+	serverClient *tdb.MysqlClient
+)
+
+func InitMysqlModel(ctx context.Context) *terror.Terror {
+	runMode = tcfg.DefaultString(tcfg.LocalKey("RUN_MODE"), constant.RunModeRelease)
+
+	serverDsn := tcfg.DefaultString(fmt.Sprintf("%s::%s", runMode, tcfg.LocalKey("SERVER_MYSQL_DSN")), "")
+	if serverDsn == "" {
+		errMsg := tlog.E(ctx).Msgf("init mysql model err (server mysql dsn illegal).")
+
+		errx := terror.NewRawTerror(ctx, terror.ErrConfIllegal("server mysql dsn"), errMsg)
+
+		return errx
+	}
+
+	var err error
+
+	if runMode == constant.RunModeDebug {
+		serverClient, err = tdb.NewMysqlClientWithLog(ctx, serverDsn)
+	} else {
+		serverClient, err = tdb.NewMysqlClient(ctx, serverDsn)
+	}
+	if err != nil {
+		errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%s) err (new mysql client %v).",
+			serverDsn, err)
+
+		errx := terror.NewRawTerror(ctx, err, errMsg)
+
+		return errx
+	}
+
+	maxIdleConns := tcfg.DefaultInt(tcfg.LocalKey("MYSQL_MAX_IDLE_CONNS"), 10)
+
+	err = serverClient.SetMaxIdleConns(maxIdleConns)
+	if err != nil {
+		errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%d) err (set max idle conns %v).",
+			maxIdleConns, err)
+
+		errx := terror.NewRawTerror(ctx, err, errMsg)
+
+		return errx
+	}
+
+	maxOpenConns := tcfg.DefaultInt(tcfg.LocalKey("MYSQL_MAX_OPEN_CONNS"), 100)
+
+	err = serverClient.SetMaxOpenConns(maxOpenConns)
+	if err != nil {
+		errMsg := tlog.E(ctx).Err(err).Msgf("init mysql model (%d) err (set max open conns %v).",
+			maxOpenConns, err)
+
+		errx := terror.NewRawTerror(ctx, err, errMsg)
+
+		return errx
+	}
+
+	return nil
+}
--- template/internal/model/redis/redis.go
+++ template/internal/model/redis/redis.go
@@ -6,58 +6,56 @@
  * @Date: 2023/11/15 21:23
  */
 
- package redmodel
+package redmodel
 
- import (
-	 "context"
-	 "fmt"
- 
-	 "github.com/choveylee/tcfg"
-	 "github.com/choveylee/tdb"
-	 "github.com/choveylee/terror"
-	 "github.com/choveylee/tlog"
-	 "github.com/go-redis/redis/v8"
- 
-	 "{{domain}}/{{app_name}}/internal/const"
- )
- 
- var (
-	 runMode string
- 
-	 serverClient *tdb.RedisClient
- )
- 
- func GetCronRedisClient(ctx context.Context) *redis.Client {
-	 return serverClient.Client(ctx)
- }
- 
- func InitRedisModel(ctx context.Context) *terror.Terror {
-	 runMode = tcfg.DefaultString(tcfg.LocalKey("RUN_MODE"), constant.RunModeRelease)
- 
-	 serverAddress, err := tcfg.String(fmt.Sprintf("%s::%s", runMode, tcfg.LocalKey("SERVER_REDIS_ADDRESS")))
-	 if err != nil {
-		 errMsg := tlog.E(ctx).Err(err).Msgf("init redis model (%s::%s) err (cfg string %v).",
-			 runMode, "SERVER_REDIS_ADDRESS", err)
- 
-		 errx := terror.NewRawTerror(ctx, err, errMsg)
- 
-		 return errx
-	 }
- 
-	 serverPassword := tcfg.DefaultString(fmt.Sprintf("%s::%s", runMode, tcfg.LocalKey("SERVER_REDIS_PASSWORD")), "")
-	 
-	 serverPoolSize := tcfg.DefaultInt(tcfg.LocalKey("SERVER_REDIS_POOLSIZE"), 100)
- 
-	 serverClient, err = tdb.NewRedisClient(ctx, serverAddress, serverPassword, serverPoolSize)
-	 if err != nil {
-		 errMsg := tlog.E(ctx).Err(err).Msgf("init redis model (%s, %s, %d) err (new redis client %v).",
-			 serverAddress, serverPassword, serverPoolSize, err)
- 
-		 errx := terror.NewRawTerror(ctx, err, errMsg)
- 
-		 return errx
-	 }
- 
-	 return nil
- }
- 
+import (
+	"context"
+	"fmt"
+
+	"github.com/choveylee/tcfg"
+	"github.com/choveylee/tdb"
+	"github.com/choveylee/terror"
+	"github.com/choveylee/tlog"
+	"github.com/go-redis/redis/v8"
+)
+
+var (
+	runMode string
+
+	serverClient *tdb.RedisClient
+)
+
+func GetCronRedisClient(ctx context.Context) *redis.Client {
+	return serverClient.Client(ctx)
+}
+
+func InitRedisModel(ctx context.Context) *terror.Terror {
+	runMode = tcfg.DefaultString(tcfg.LocalKey("RUN_MODE"), constant.RunModeRelease)
+
+	serverAddress := tcfg.DefaultString(fmt.Sprintf("%s::%s", runMode, tcfg.LocalKey("SERVER_REDIS_ADDRESS")), "")
+	if serverAddress == "" {
+		errMsg := tlog.E(ctx).Msgf("init redis model err (server redis address illegal).")
+
+		errx := terror.NewRawTerror(ctx, terror.ErrConfIllegal("server redis address"), errMsg)
+
+		return errx
+	}
+
+	serverPassword := tcfg.DefaultString(fmt.Sprintf("%s::%s", runMode, tcfg.LocalKey("SERVER_REDIS_PASSWORD")), "")
+
+	serverPoolSize := tcfg.DefaultInt(tcfg.LocalKey("SERVER_REDIS_POOLSIZE"), 100)
+
+	var err error
+
+	serverClient, err = tdb.NewRedisClient(ctx, serverAddress, serverPassword, serverPoolSize)
+	if err != nil {
+		errMsg := tlog.E(ctx).Err(err).Msgf("init redis model (%s, %s, %d) err (new redis client %v).",
+			serverAddress, serverPassword, serverPoolSize, err)
+
+		errx := terror.NewRawTerror(ctx, err, errMsg)
+
+		return errx
+	}
+
+	return nil
+}
